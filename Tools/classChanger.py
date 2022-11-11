25f0eeeecf3104e04bf268ace9a49a7686983f4f
Oier Mentxaka <oiermentxaka@gmail.com>
2022-11-11 12:52:20 +0100

    merge datasets

---
+++ Tools/classChanger.py
@@ -0,0 +1,16 @@
+import glob
+
+#path = r"C:\Users\eneko\GitHub\TK-VisionArtificial\KaggleCoches\train\*.txt"
+#path = "/Users/mentxaka/Github/TK-Datasets/Matriculas copy/train/*.txt"
+
+def classChanger(path):
+    files = glob.glob(path)
+
+    for file in files:
+        with open(file, 'r') as f: data = f.readlines()
+
+        newText = ""
+        for line in data:
+            newText += "1" + line[1:]
+
+        with open(file.split(".")[0] + ".txt", 'w') as file: file.write(newText)
