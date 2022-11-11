fa20713632c91d0260451c3fbf8d62cbcfd5b6d8
Oier Mentxaka <oiermentxaka@gmail.com>
2022-11-11 12:58:30 +0100

    Update mergeDatasets.py

--- Tools/mergeDatasets.py
+++ Tools/mergeDatasets.py
@@ -2,24 +2,26 @@ import shutil
 import os
 from classChanger import classChanger
 
+def mergeDatasets(datasetA, datasetB):
+    destDataset = datasetA+datasetB.split("/")[-1]
+
+    shutil.copytree(datasetB, destDataset)
+
+    destDatasetTest = destDataset + "/test/"
+    classChanger(destDatasetTest + "*.txt")
+
+    destDatasetTrain = destDataset + "/train/"
+    classChanger(destDatasetTrain + "*.txt")
+
+    aDatasetTest = datasetA+"/test/"
+    aDatasetTrain = datasetA+"/train/"
+
+    for file in os.listdir(aDatasetTest):
+        shutil.copy(aDatasetTest + file, destDatasetTest)
+
+    for file in os.listdir(aDatasetTrain):
+        shutil.copy(aDatasetTrain + file, destDatasetTrain)
+
 datasetA = "/Users/mentxaka/Github/TK-Datasets/Caras"
 datasetB = "/Users/mentxaka/Github/TK-Datasets/Matriculas"
-
-destDataset = datasetA+datasetB.split("/")[-1]
-
-shutil.copytree(datasetB, destDataset)
-
-destDatasetTest = destDataset + "/test/"
-classChanger(destDatasetTest + "*.txt")
-
-destDatasetTrain = destDataset + "/train/"
-classChanger(destDatasetTrain + "*.txt")
-
-aDatasetTest = datasetA+"/test/"
-aDatasetTrain = datasetA+"/train/"
-
-for file in os.listdir(aDatasetTest):
-    shutil.copy(aDatasetTest + file, destDatasetTest)
-
-for file in os.listdir(aDatasetTrain):
-    shutil.copy(aDatasetTrain + file, destDatasetTrain)
+mergeDatasets(datasetA,datasetB)
